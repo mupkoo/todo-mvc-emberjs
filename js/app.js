0cc84809f1971e91798725b96a963d2368613dd0
Mirko Akov <mupkoo@gmail.com>
2014-04-03 13:20:01 +0200

    Change the Fixture adaptar to Firebase adapter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
+    firebase: new Firebase('https://glaring-fire-8506.firebaseio.com')
+});
