a00fe49cd311670bd9ea87e4047d5fedd3ea9af7
Mirko Akov <mupkoo@gmail.com>
2014-04-03 18:48:58 +0200

    Rollback the to personal firebase api and add initial json file

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 window.Todos = Ember.Application.create();
 
-// Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
-// Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
-//     firebase: new Firebase('https://glaring-fire-8506.firebaseio.com')
-// });
-
 Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
-    firebase: new Firebase("https://todomvc-ember.firebaseio-demo.com/")
+    firebase: new Firebase('https://glaring-fire-8506.firebaseio.com')
 });
